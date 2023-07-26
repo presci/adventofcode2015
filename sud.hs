15201f091fd2012facd1a1b2c942dfe8b8b2b7cf
prasad iyer <p.c.iyer@gmail.com>
2023-07-25 22:22:15 -0500

    Update sud.hs

--- sud.hs
+++ sud.hs
@@ -1,10 +1,9 @@
 import SudUtils
-
 import qualified Data.Vector as V
 import Data.Maybe
-import Data.List (sort, group)
--- / end initialization code
-
+import Data.List (sort, group, foldl')
+import Data.Maybe (Maybe(Nothing))
+import Data.Int (Int)
 
 solve :: Board -> Board
 solve vec = V.imap update vec
@@ -17,39 +16,44 @@ solve vec = V.imap update vec
             [x] -> Cell x
             a -> Cells a
 
-
 getCellAt::(Int, Int) -> Board -> Maybe Cell
 getCellAt (x, y) board = do
     row <- board V.!? x
     row V.!? y
 
-solve02 :: Board -> Board
-solve02 board = let emptycell =  findNextEmptyCell board
-    in helper emptycell
+solve02 :: Board -> Maybe Board
+solve02 board = case findNextEmptyCell board of
+        Just a -> case getCellAt a board of
+            Just (Cells xss) -> helper a xss board
+            _ -> error "done"
+        _ -> Just board
     where
-        helper :: Maybe (Int, Int) -> Board
-        helper (Just coord@(x, y)) = case getCellAt coord board of
-                                    (Just (Cells xss)) -> isValid coord xss board
-                                    _ -> board
-        helper _ = board
-        isValid::(Int, Int) -> [Int] -> Board -> Board
-        isValid _ [] brd = brd
-        isValid a@(x1, y1) (x:xss) brd = if x `notElem` getskiplist a brd then solve02 (brd V.// [(x1, (brd V.! x1) V.// [(y1, Cell x)])]) else isValid a xss brd
+        helper :: (Int, Int) -> [Int] -> Board -> Maybe Board
+        helper a xss brd = foldl' (tryValue a )  (Just brd) xss
+        tryValue:: (Int, Int) -> Maybe Board -> Int -> Maybe Board
+        tryValue a bd@(Just brd) val = if val `notElem` getskiplist a brd then solve02 (newbrd a brd val) else bd
+        tryValue _ _ _ = Nothing
+        newbrd ::(Int, Int) -> Board -> Int -> Board
+        newbrd (x, y) brd val = brd V.// [(x, (brd V.! x) V.// [(y, Cell val)])]
+
+test02:: Maybe Int -> Int -> Maybe Int
+test02 (Just x) arg0 = if  arg0  >= x then Just arg0 else Nothing
+test02 _ arg0 = Just arg0
 
 test05::Board
-test05 = conv2 . V.fromList $ map V.fromList [[8,2,5,4,7,1,3,9,6],
-                                              [0,0,0,3,2,6,5,7,8],
-                                              [0,0,0,9,8,5,2,4,1],
-                                              [0,0,0,7,4,3,8,0,2],
-                                              [0,0,0,5,9,8,4,0,7],  -- remove 9 here and it fails
-                                              [0,0,0,6,0,2,0,3,5],
-                                              [2,6,3,1,5,0,0,8,0],
-                                              [9,4,8,2,0,7,0,0,3],
-                                              [0,0,0,8,0,4,0,2,9]]
+test05 = conv2 . V.fromList $ map V.fromList [[0,0,0,0,0,6,0,0,8], -- 0,0,0,0,0,6,0,0,8  149536278
+                                              [0,0,0,7,8,0,1,0,0], -- 0,0,0,7,8,0,1,0,0  526789134
+                                              [0,0,3,0,0,0,0,0,5], -- 0,0,3,0,0,0,0,0,5  783241965
+                                              [0,0,1,4,0,0,6,9,2], -- 0,0,1,4,0,0,6,9,2  851473692
+                                              [2,3,7,6,0,5,0,0,0], -- 0,0,0,6,0,5,0,0,0  237695841
+                                              [6,9,4,8,1,2,7,5,3], -- 6,9,4,0,0,2,7,0,0  694812753
+                                              [4,6,8,1,5,7,3,2,9], -- 4,0,0,0,0,0,3,0,0  468157329
+                                              [3,7,5,9,2,8,4,1,6]] -- 3,0,0,9,0,0,0,0,0  375928416
+
+
+
 main::IO()
 main = do
-    let a =   solve $ test05
+    let a =  solve02 . solve $ test05
     print a
     print "---------"
-    let g =  solve02 a
-    print g
